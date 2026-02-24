177137361e13a33b3fdfb6c59298e992537058cb
Curryllo <842545@unizar.es>
2026-02-24 10:02:42 +0100

    Refactorización del proyecto, arquitectura limpia y hexagonal

---
+++ app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    kotlin("plugin.spring")
+}
+
+group = "org.unizar"
+version = "0.0.1-SNAPSHOT"
+description = "TFG_backend"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.security:spring-security-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,6 @@
 rootProject.name = "TFG_backend"
+
+include("core")
+include("repositories")
+include("delivery")
+include("app")
