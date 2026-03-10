e95aa96f1616802faaf11cd84edaafe825066651
Curryllo <842545@unizar.es>
2026-03-10 12:32:10 +0100

    Añadido registro de coordenadas en todos las entradas en la BD

--- delivery/src/main/kotlin/org/unizar/tfg_backend/infraestructure/delivery/Controlador.kt
+++ delivery/src/main/kotlin/org/unizar/tfg_backend/infraestructure/delivery/Controlador.kt
@@ -2,7 +2,12 @@
 
 package org.unizar.tfg_backend.infraestructure.delivery
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,6 +16,8 @@ import org.unizar.tfg_backend.core.usecases.LogFormularioHumanoUseCase
 import org.unizar.tfg_backend.core.usecases.LogFormularioMonitoreoUseCase
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.util.UriComponentsBuilder
 import org.unizar.tfg_backend.core.usecases.ObtenerFormulariosHumanosUseCase
 import java.time.LocalDate
 
@@ -18,6 +25,7 @@ interface Controlador {
     fun guardarFormularioHumano(datos: FormularioHumanosIn, request: HttpServletRequest) : ResponseEntity<Any>
     fun obtenerDatosHumanos(request: HttpServletRequest) : ResponseEntity<Any>
     fun guardarFormularioMonitoreo(datos: FormularioMonitoreoIn, request: HttpServletRequest) : ResponseEntity<Any>
+    fun completarDatosGeograficos(datos: FormularioMonitoreoIn) : FormularioMonitoreoIn
 }
 
 
@@ -43,12 +51,63 @@ data class FormularioMonitoreoIn(
     val longitud: Double? = null
 )
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class NominatimSearchResponse(
+    val lat: String = "",
+    val lon: String = ""
+)
+
+data class NominatimReverseResponse(
+    @JsonProperty("display_name")
+    val displayName: String = ""
+)
+
 @RestController
 class ControladorImpl(
     val logFormularioHumanoUseCase: LogFormularioHumanoUseCase,
     val obtenerFormulariosHumanosUseCase: ObtenerFormulariosHumanosUseCase,
     val logFormularioMonitoreoUseCase: LogFormularioMonitoreoUseCase
 ) : Controlador {
+    private val restTemplate = RestTemplate()
+    private val nominatimUrl = "https://nominatim.openstreetmap.org"
+
+    override fun completarDatosGeograficos(datos: FormularioMonitoreoIn): FormularioMonitoreoIn {
+        val headers = HttpHeaders()
+        headers.set("User-Agent", "TFG_IngInfor_Monitoreo")
+        val entity = HttpEntity<String>(headers)
+
+        if (!datos.lugarRecogida.isNullOrBlank() && datos.latitud == null) {
+            val url = UriComponentsBuilder.fromHttpUrl("$nominatimUrl/search")
+                .queryParam("q", datos.lugarRecogida)
+                .queryParam("format", "json")
+                .queryParam("limit", 1)
+                .build().toUriString()
+
+            val response = restTemplate.exchange(url, HttpMethod.GET, entity, Array<NominatimSearchResponse>::class.java)
+            val body = response.body
+            if (!body.isNullOrEmpty()) {
+                return datos.copy(
+                    latitud = body[0].lat.toDouble(),
+                    longitud = body[0].lon.toDouble()
+                )
+            }
+        }
+
+        if (datos.latitud != null && datos.longitud != null && datos.lugarRecogida.isNullOrBlank()) {
+            val url = UriComponentsBuilder.fromHttpUrl("$nominatimUrl/reverse")
+                .queryParam("lat", datos.latitud)
+                .queryParam("lon", datos.longitud)
+                .queryParam("format", "json")
+                .build().toUriString()
+
+            val response = restTemplate.exchange(url, HttpMethod.GET, entity, NominatimReverseResponse::class.java)
+
+            response.body?.let {
+                return datos.copy(lugarRecogida = it.displayName)
+            }
+        }
+        return datos
+    }
     @PostMapping(value = ["/api/formHumanos"])
     override fun guardarFormularioHumano(
         @RequestBody datos: FormularioHumanosIn, request: HttpServletRequest): ResponseEntity<Any> {
@@ -84,17 +143,21 @@ class ControladorImpl(
     override fun guardarFormularioMonitoreo(
         @RequestBody datos: FormularioMonitoreoIn, request: HttpServletRequest): ResponseEntity<Any> {
         println("Ha entrado")
+        val datosCompletos = completarDatosGeograficos(datos)
+
         val resultado = logFormularioMonitoreoUseCase.log(
-            l = datos.lugarRecogida,
-            v = datos.vector,
-            e = datos.enfermedad,
-            f = datos.fecha,
-            n = datos.numero,
-            g = datos.genero,
-            lat = datos.latitud,
-            long = datos.longitud
+            l = datosCompletos.lugarRecogida,
+            v = datosCompletos.vector,
+            e = datosCompletos.enfermedad,
+            f = datosCompletos.fecha,
+            n = datosCompletos.numero,
+            g = datosCompletos.genero,
+            lat = datosCompletos.latitud,
+            long = datosCompletos.longitud
         )
 
         return ResponseEntity.status(HttpStatus.CREATED).body(resultado)
     }
+    
+
 }
